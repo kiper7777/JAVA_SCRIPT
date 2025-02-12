ddd5b7eb03d7e6ddff1a43f49f66dd6c03db8cdd
Dimas Kiperman <126020343+kiper7777@users.noreply.github.com>
2025-02-12 21:53:42 +0000

    Add files via upload

---
+++ Project1/header.php
@@ -0,0 +1,25 @@
+<header>
+    <div class="container">
+        <div class="logo-nav">
+            <div class="logo">
+                <img src="/image/logo.png" alt="Profitrade Logo">
+            </div>
+            <div class="burger" id="burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <nav class="menu" id="menu">
+                <ul>
+                    <li><a href="#">Price Policy</a></li>
+                    <li><a href="#">Help Center</a></li>
+                    <li><a href="#">Join our Discord</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="auth-buttons">
+            <button class="login">Log in</button>
+            <!-- <button class="signup" id="signupButton">Sign up</button> -->
+        </div>
+    </div>
+</header>
